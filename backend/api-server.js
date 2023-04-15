8d6ffbd24cef9fc5edd85a73232f44cf0a5c2801
seorim <hhyjs2r@gmail.com>
2023-04-15 23:43:10 +0900

    [feat] 서버와 통신해서 로그인,Id 정보 주고받기

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 app.get('/api/account', (req, res) => {
-  res.send({
-    mid:3,
-    memberNumber:"윤서림"
-  })
+  // res.send({
+  //   mid:3,
+  //   memberName:"윤서림"
+  // })
+
+    res.send(401)
+})
+
+app.post('/api/account', (req, res) => {
+const loginId = req.body.loginId
+const loginPw = req.body.loginPw
+console.log(loginId,loginPw)
+res.json({ message: '제출했어요' });
 })
 
 app.listen(port, () => {
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,15 +5,17 @@
 
     <div v-else>
     <label for="loginId">
-    <input type="text" id="loginId"/>
+     <span>아이디</span>
+    <input type="text" id="loginId" v-model="state.form.loginId"/>
   </label>
 
      <label for="loginPw">
-    <input type="password" id="loginPw"/>
+      <span>패스워드</span>
+    <input type="password" id="loginPw" v-model="state.form.loginPw"/>
   </label>
 
   <br/>
-  <button>로그인</button>
+  <button @click="handleSubmit">로그인</button>
 
 </div>
   </div>
@@ -29,12 +31,32 @@ const state = reactive({
    mid:null,
    memberName : ""
    } ,
+   form:{
+   loginId : "",
+   loginPw:""
+   },
 loggedIn : false})
-console.log(state)
+
+const handleSubmit = () =>{
+  console.log('제출')
+  const args = {
+   loginId: state.form.loginId,
+   loginPw: state.form.loginPw
+  }
+
+  axios.post('/api/account', args).then((res)=>{
+    console.log(res)
+  })
+
+
+
+}
 
 axios.get('/api/account').then((res)=>{
   console.log(res.data)
   state.account = res.data
 })
+
+// return { state, submit }
 
 </script>
