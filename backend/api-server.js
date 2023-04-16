83888b733800a18bc208446b3538fa6075061d92
seorim <hhyjs2r@gmail.com>
2023-04-16 14:09:07 +0900

    [feat] jwt 변경

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken') //jwt임포트
+const jwtKey = "abc1234567"; //암호
+
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 
 const membesList = [
@@ -11,12 +16,21 @@ const membesList = [
 ]
 
 app.get('/api/account', (req, res) => {
-  // res.send({
-  //   mid:3,
-  //   memberName:"윤서림"
-  // })
+    if (req.cookies && req.cookies.token) {
+//검증한다.
+        jwt.verify(req.cookies.token, jwtKey, (err, decoded) => {
+            if (err) {
+                return res.sendStatus(401);
+            }
 
-    res.send(401)
+            res.send(decoded); //최종결과,
+
+            //1. 쿠키가 있으면 , 2. 쿠키값을 검증해서 , 3. 에러가 있으면 에러를 뱉고, 없으면 값을 뱉는다.
+        })
+    }
+    else {
+        res.sendStatus(401);
+    }
 })
 
 app.post('/api/account', (req, res) => {
@@ -25,17 +39,32 @@ const loginId = req.body.loginId
 const loginPw = req.body.loginPw
 
 const member = membesList.find((m)=> m.loginId === loginId && m.loginPw === loginPw )
-console.log(member)
 
 if (member) {
     const options = {
-      domain: "localhost", // 쿠키가 유효한 도메인을 지정. 예를 들어, ".example.com"와 같이 하위 도메인까지 포함하여 지정할 수 있습니다. 기본값은 현재 도메인입니다.
+      domain: "localhost", // 쿠키가 유효한 도메인을 지정.
       path: "/", // 쿠키가 유효한 경로를 지정.
-      httpOnly: true, // JavaScript를 통해 쿠키에 접근하지 못하도록 합니다. 이를 통해 XSS(Cross-Site Scripting) 공격을 방지할 수 있다. 기본값은 false입니다.
+      httpOnly: true, // JavaScript를 통해 쿠키에 접근하지 못하도록 한다. 이를 통해 XSS(Cross-Site Scripting) 공격을 방지할 수 있다. 기본값은 false.
       sameSite: "strict" // 쿠키가 SameSite 속성을 가져야 하는지 지정 SameSite 속성은 쿠키가 Cross-Site 요청에서만 전송되도록 제한하는 것으로, CSRF(Cross-Site Request Forgery) 공격을 방지할 수 있다.
-      // "strict", "lax", "none" 중 하나의 값을 지정할 수 있고,. 기본값은 "lax"입니다.
+      // "strict", "lax", "none" 중 하나의 값을 지정할 수 있고,. 기본값은 "lax"이다.
     }
-    res.cookie("account", JSON.stringify(member), options);
+
+   const token = jwt.sign(//JWT(Json Web Token)을 생성하는 함수
+       {
+       mid: member.mid,
+       memberName: member.memberName,
+        },
+
+         jwtKey, //JWT Signature를 생성할 때 사용하는 비밀키를 전달.
+         //이를 통해 서버에서 생성한 토큰임을 검증할 수 있다.
+
+       {
+         expiresIn: "15m",
+        issuer: "africalib"
+       }
+   );
+
+        res.cookie("token", token, options); //멤버 그대로를 json파싱해서 넣어주는 것이 아니라, 저 token값으로 넣어준다.
     res.send(member);
   } else {
     res.sendStatus(404);
